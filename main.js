112c498f39077f70046f9e57ae66a121240da855
carvajalejandro <alejandro.carvajal@mycase.com>
2022-09-04 08:51:40 -0600

    Can choose second operator after calculating

--- main.js
+++ main.js
@@ -11,10 +11,15 @@ const currenrOperandTextElement=document.querySelector('[data-current-operand]')
 const numberBtns=document.querySelectorAll('[data-number]')
 const operationBtns=document.querySelectorAll('[data-operation]')
 const equalsButton=document.querySelector('[data-equals]')
-equalsButton.addEventListener('click', calculate);
+equalsButton.addEventListener('click', ()=>{
+    if(currentNum!= '' && previousNum!=''){
+        calculate();
+    }
+});
+
 const deleteButton=document.querySelector('[data-delete]')
 const allClearButtonr=document.querySelector('[data-all-clear]')
-allClearButtonr.addEventListener('click', allClear);
+allClearButtonr.addEventListener('click', clearCalc);
 
 //Passes the number clicked on to the hanldeNumber() function
 numberBtns.forEach(btn =>{
@@ -26,9 +31,7 @@ numberBtns.forEach(btn =>{
 //Passes the operator clicked on to the hanldeNumber() function
 operationBtns.forEach(btn =>{
     btn.addEventListener('click', (e) =>{
-        if(previousNum===''){
-            handleOperator(e.target.textContent)   
-        }
+        handleOperator(e.target.textContent)   
     });
 });
 //Passes the number clicked into the currentOperand display
@@ -40,29 +43,51 @@ function handleNumber(number){
 }
 //Passes the operator and the currentOperand display & switches it to previousOperand & resets currentOperand
 function handleOperator(op){
+    if(previousNum===''){
+        previousNum=currentNum;
+        operatorCheck(op);
+    }else if (currentNum===''){
+        operatorCheck(op);
+    }else{
+    calculate();
     operator=op;
-    previousNum=currentNum;
+    currenrOperandTextElement.textContent='0';
     previousOperandTextElement.textContent=previousNum +' '+ operator;
-    currentNum='';
-    currenrOperandTextElement.textContent='';
-    
+    }
 }
 
+function operatorCheck(text){
+    operator=text;
+    previousOperandTextElement.textContent=previousNum +' '+ operator;
+    currenrOperandTextElement.textContent='0';
+    currentNum='';
+}
 function calculate(){
     previousNum= Number(previousNum);
     currentNum= Number(currentNum);
 
     if(operator==='+'){
         previousNum= previousNum + currentNum;
+    } else if (operator==='-'){
+        previousNum= previousNum - currentNum;
+    } else if (operator==='*'){
+        previousNum= previousNum * currentNum;
+    } else if (operator==='÷'){
+        previousNum= previousNum / currentNum;
     }
+  previousNum= previousNum.toString();
+  displayResults();
+}
+function displayResults(){
     previousOperandTextElement.textContent='';
     currenrOperandTextElement.textContent=previousNum;
+    operator='';
 }
-//deletes everything function
-function allClear (currentNum,previousNum,operator){
-    currentNum=''
-    previousNum=''
-    operator=''
-    currenrOperandTextElement.textContent=currentNum;
-    previousOperandTextElement.textContent=previousNum;
+// clear everything function
+function clearCalc (){
+    currentNum='';
+    previousNum='';
+    operator='';
+    currenrOperandTextElement.textContent='0';
+    previousOperandTextElement.textContent='';
 }
